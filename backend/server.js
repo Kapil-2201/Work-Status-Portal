bb19698c847e3e8713e2d6f5d4f89b83a22884da
KAPIL D <kapildhanasekaran@gmail.com>
2025-03-04 14:13:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const dotenv = require('dotenv');
-
-// // Load environment variables
-// dotenv.config();
-
-// const app = express();
-// const port = process.env.PORT || 5173;  // Ensure this is 5173
-
-// // Import routes
-// const staffRouter = require('./routes/staff');
-// const taskRouter = require('./routes/task');
-
-// // Middleware
-// const corsOptions = {
-//   origin: 'http://localhost:3000',  // This should match your React app's URL
-//   optionsSuccessStatus: 200,
-// };
-
-// // Apply CORS middleware
-// app.use(cors(corsOptions));
-// app.use(express.json());
-
-// // Use routes
-// app.use('/api/staff', staffRouter);
-// app.use('/api/task', taskRouter);
-
-// // Connect to MongoDB
-// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB connection established'))
-//   .catch((error) => console.log('MongoDB connection error:', error));
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server is running on port: ${port}`);
-// });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -53,18 +15,19 @@ const taskRouter = require('./routes/task');
 const attendanceRouter = require('./routes/attendance');
 
 
-// Middleware
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://work-status-portal.vercel.app'],  
-  optionsSuccessStatus: 200,
-};
+// // Middleware
+// const corsOptions = {
+//   origin: ['http://localhost:3000', 'https://work-status-portal.vercel.app'],  
+//   optionsSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+
+app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
 
 
 
 // Apply CORS middleware
-app.use(cors(corsOptions));
 app.use(express.json());
 
 // Use routes
